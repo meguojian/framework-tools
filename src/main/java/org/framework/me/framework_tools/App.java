8c77c248ece78cb09dd3c9f214b1244f8d05d3b0
gc <meguojian@163.com>
2013-04-06 20:12:52 +0800

    init

---
+++ src/main/java/org/framework/me/framework_tools/App.java
@@ -0,0 +1,13 @@
+package org.framework.me.framework_tools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
